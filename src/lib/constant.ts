ab4fc71d267e8fc779c93d53327092aab0abefab
Lawal Abubakar Babatunde <utility4all@gmail.com>
2025-06-20 15:38:18 +0100

    Chore: update addresses

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -20,58 +20,28 @@ export const equalDistributionType = [
 ] as const;
 
 export const MAINNET_SUPPORTED_TOKENS = Object.freeze({
-  STRK: {
-    symbol: "STRK",
-    address:
-      "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
-    decimals: 18,
-  },
   USDC: {
     symbol: "USDC",
     address:
-      "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+      "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
     decimals: 6,
-  },
-  ETH: {
-    symbol: "ETH",
-    address:
-      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-    decimals: 18,
-  },
-  USDT: {
-    symbol: "USDT",
-    address:
-      "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
-    decimals: 6,
-  },
+  }
 }) as Readonly<Record<string, Readonly<TokenOption>>>;
 
 export const TESTNET_SUPPORTED_TOKENS = Object.freeze({
-  STRK: {
-    symbol: "STRK",
-    address:
-      "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
-    decimals: 18,
-  },
   USDC: {
     symbol: "USDC",
     address:
       "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
     decimals: 6,
-  },
-  ETH: {
-    symbol: "ETH",
-    address:
-      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-    decimals: 18,
-  },
+  }
 }) as Readonly<Record<string, Readonly<TokenOption>>>;
 
 export const TESTNET_CONTRACT_ADDRESS =
-  "0x3aDF45db77F1B69Ab7b08745d7d2d3E0F36833ab";
+  "0xaa0939b802060785c541553911caa6781c41f5d5";
 
 export const MAINNET_CONTRACT_ADDRESS =
-  "0x3aDF45db77F1B69Ab7b08745d7d2d3E0F36833ab";
+  "0xaa0939b802060785c541553911caa6781c41f5d5";
 
 export const distributionState = [
   "process-started",
