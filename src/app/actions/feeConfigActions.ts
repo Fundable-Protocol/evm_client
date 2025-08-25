c610f844710ed6e5cd718629954d144f835b1d9d
Lawal Abubakar Babatunde <utility4all@gmail.com>
2025-08-25 12:24:45 +0300

    feat: enhance network parameter handling in getProtocolFee function
    - Convert network parameter to uppercase for consistent database queries
    - Add proper type casting to ensure 'mainnet' | 'testnet' format

--- src/app/actions/feeConfigActions.ts
+++ src/app/actions/feeConfigActions.ts
@@ -7,39 +7,41 @@ import { feeConfigModel } from "../../../db/schema";
 import { supportedChainName, supportedNetwork } from "@/lib/constant";
 
 export async function getProtocolFee(
-    network: (typeof supportedNetwork)[number] = "testnet",
-    chainName: (typeof supportedChainName)[number] = "Starknet"
-  ) {
-    try {
-      const andQuery = and(
-        eq(feeConfigModel.network, network),
-        ilike(feeConfigModel.chainName, chainName)
-      );
-  
-      const { data: protocolFee, error: protocolFeeError } = await tryCatch(
-        db
-          .select({
-            id: feeConfigModel.id,
-            amount: feeConfigModel.amount,
-            network: feeConfigModel.network,
-            chainName: feeConfigModel.chainName,
-          })
-          .from(feeConfigModel)
-          .where(andQuery)
-      );
-  
-      return {
-        success: !protocolFeeError,
-        data: {
-          protocolFee: Number(protocolFee?.[0]?.amount ?? 0),
-        },
-        error: protocolFeeError,
-      };
-    } catch (error) {
-      return {
-        success: false,
-        error,
-      };
-    }
-  }
+  network: (typeof supportedNetwork)[number] = "testnet",
+  chainName: (typeof supportedChainName)[number] = "Starknet"
+) {
+  try {
+    const andQuery = and(
+      eq(
+        feeConfigModel.network,
+        network?.toUpperCase() as "mainnet" | "testnet"
+      ),
+      ilike(feeConfigModel.chainName, chainName)
+    );
 
+    const { data: protocolFee, error: protocolFeeError } = await tryCatch(
+      db
+        .select({
+          id: feeConfigModel.id,
+          amount: feeConfigModel.amount,
+          network: feeConfigModel.network,
+          chainName: feeConfigModel.chainName,
+        })
+        .from(feeConfigModel)
+        .where(andQuery)
+    );
+
+    return {
+      success: !protocolFeeError,
+      data: {
+        protocolFee: Number(protocolFee?.[0]?.amount ?? 0),
+      },
+      error: protocolFeeError,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error,
+    };
+  }
+}
